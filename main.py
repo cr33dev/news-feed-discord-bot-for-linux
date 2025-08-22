1aee2b32d514a2a4108fd4bacbdb4eae27bcebaf
Jackson Thrasher <jacksonthrasher@airmail.cc>
2025-08-22 00:24:33 -0400

    camelCased everything :^)

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ titleOnly = False   # Forces embeds to only show article title (and author/pub d
 forceList = False   # Forces emebds to always show previous stories instead of content description  (False by default)
 appendList = False  # Forces feeds with valid content descriptions to also display previous stories (False by default)
 refreshRate = 1800  # Determines how often the bot checks the feed list. Measured in seconds.       (1800 by default)
+rssUrlList = ["https://archlinux.org/feeds/news/","https://blog.linuxmint.com/?feed=rss2","https://planet.gnu.org/rss20.xml","https://bits.debian.org/feeds/feed.rss","https://rss.slashdot.org/Slashdot/slashdotMain","https://lunduke.substack.com/feed"]
 
 ### USERS WHO MODIFY CODE BEYOND THIS POINT: BEWARE YE ARE ENTERING UNDOCUMENTED TERRITORY ###
 
@@ -51,31 +52,30 @@ headers = {
         "Chrome/115.0.0.0 Safari/537.36"
     )
 }
-# add RSS feed links. MUST BE A URL POINTING DIRECTLY TO RSS FEED OR ELSE IT WON'T WORK.
-rss_url_list = ["https://archlinux.org/feeds/news/","https://blog.linuxmint.com/?feed=rss2","https://planet.gnu.org/rss20.xml","https://bits.debian.org/feeds/feed.rss","https://rss.slashdot.org/Slashdot/slashdotMain","https://lunduke.substack.com/feed"]
+
 # what does this even do? - seafer6969
-newest_value_list = ["value","value","value","value","value","value"]
+newestValueList = ["value","value","value","value","value","value"]
 # main
 async def main_function(chan_id):
     channel = client.get_channel(chan_id)
     # various variables needed for the previous story list maker
-    global rss_url_list, newest_value_list, temp_all_feed_list
+    global rssUrlList, newestValueList, previousArticlesContainer
     loop_allways_active = True
     while loop_allways_active:
-        for i in range(len(rss_url_list)): # loop for going through all RSS feeds
-            index_number = i
-            temp_all_feed_list = [] # reset the temporary feed list
-            response = requests.get(rss_url_list[index_number], headers=headers)
+        for i in range(len(rssUrlList)): # loop for going through all RSS feeds
+            feedIndex = i
+            previousArticlesContainer = [] # reset the temporary feed list
+            response = requests.get(rssUrlList[feedIndex], headers=headers)
             temp = feedparser.parse(response.content)
             # lopp for making previous stories list
             for entry in temp.entries[:6]:
-                temp_all_feed_list.append("[" + entry.title +"]" + "(" + entry.link + ")")
-            if newest_value_list[index_number] != temp_all_feed_list[0]:
+                previousArticlesContainer.append("[" + entry.title +"]" + "(" + entry.link + ")")
+            if newestValueList[feedIndex] != previousArticlesContainer[0]:
                 #this will be referenced in embedFValue a lot
                 embedList = "\n".join(
-                        [temp_all_feed_list[1],
-                         temp_all_feed_list[2],
-                         temp_all_feed_list[3],
+                        [previousArticlesContainer[1],
+                         previousArticlesContainer[2],
+                         previousArticlesContainer[3],
                         ]
                 )
                 # parse feed elements to variables usable by discord.Embed
@@ -168,9 +168,9 @@ async def main_function(chan_id):
                 embed.add_field(name = embedFName, value = embedFValue)
                 # send message, reset previous stories counter.
                 await channel.send(embed=embed)
-                newest_value_list[index_number] = temp_all_feed_list[0]
+                newestValueList[feedIndex] = previousArticlesContainer[0]
             else:
-                print(f"nothing to add for {rss_url_list[index_number]}")
+                print(f"nothing to add for {rssUrlList[feedIndex]}")
         await asyncio.sleep(refreshRate)
 # start bot
 @client.event
@@ -185,8 +185,8 @@ async def on_ready():
     if forceList:   print("DEBUG: Option 'forceList' is enabled: descriptions won't be parsed!")
     if appendList:  print("DEBUG: Option 'appendList' is enabled: 'embedList' will be shown regardless of 'entry.description' contents!")
     print("DEBUG: Option refreshRate is set to " + str(refreshRate) + ". Bot will scan RSS feeds every " + str(refreshRate / 60) + " minutes!")
-    print("\nThere are " + str(len(rss_url_list)) + " entries in rss_url_list.")
-    print("Raw contents of rss_url_list is \n" + str(rss_url_list))
+    print("\nThere are " + str(len(rssUrlList)) + " entries in rssUrlList.")
+    print("Raw contents of rssUrlList is \n" + str(rssUrlList))
     print("\nFEED DEBUG DATA: TITLE, LINK, AUTHOR, PUBLISHED, IMAGE, DESCRIPTION:")
     # print intial information to discord channel
     channel = client.get_channel(CHANNEL_ID)
@@ -198,8 +198,8 @@ async def on_ready():
             "Option: `appendList` is set to `" + str(appendList) + "`.\n" +
             "Option: `refreshRate` is set to `" + str(refreshRate) + "` seconds."
             )
-        await channel.send("There are " + str(len(rss_url_list)) + " entries in `rss_url_list`")
-        await channel.send("Raw contents of `rss_url_list` is:\n`" + str(rss_url_list) + "`")
+        await channel.send("There are " + str(len(rssUrlList)) + " entries in `rssUrlList`")
+        await channel.send("Raw contents of `rssUrlList` is:\n`" + str(rssUrlList) + "`")
     else:
         print(f"DEBUG: Could not find channel with ID {CHANNEL_ID}")
     asyncio.create_task(main_function(CHANNEL_ID))
